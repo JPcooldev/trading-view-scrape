fa9d4f8c6f3a0d57892afb295163ca117e4581e4
JPcooldev <100166799+JPcooldev@users.noreply.github.com>
2023-05-28 13:23:07 +0200

    Update tradingview_scrape_safari.py

--- tradingview_scrape_safari.py
+++ tradingview_scrape_safari.py
@@ -1,10 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
+#from selenium.webdriver.support import expected_conditions as EC
+#from selenium.webdriver.support.ui import WebDriverWait
 import re
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import time
 
